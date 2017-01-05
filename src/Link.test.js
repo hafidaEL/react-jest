8198a6a161d00cd952e1db3a07091bad5186aa37
hafida <oumnawfal@gmail.com>
2017-01-05 21:27:13 +0100

    composant Link

---
+++ src/Link.test.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import Link from './Link';
+import renderer from 'react-test-renderer';
+
+test('Le lien change de classe au hover', () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseEnter();
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseLeave();
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
+<a href="http://facebook.com">Faceblok</a>
